82edbf92950b39d79a037b3c8e0eae4604dcdc05
Gonzalo Ocaranza <gonzaoca09@gmail.com>
2025-07-27 21:53:29 -0300

    feat: Add Header, Card, Hero, and Projects components; remove old Hero and Welcome components

---
+++ src/components/base/Footer.tsx
@@ -0,0 +1,10 @@
+// Export the Footer component as default
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 HGO - Portfolio. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
